e10386b7acc4a21ac74772329f12c2dbde56795e
Jiahao <1332661444@qq.com>
2020-12-26 19:13:46 +0800

    加入mp代码

---
+++ eladmin-system/src/main/java/me/zhengjie/modules/test/service/BlogArticleService.java
@@ -0,0 +1,12 @@
+package me.zhengjie.modules.test.service;
+
+import me.zhengjie.modules.test.domain.BlogArticle;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface BlogArticleService extends IService<BlogArticle> {
+
+
+}
+
+
+
