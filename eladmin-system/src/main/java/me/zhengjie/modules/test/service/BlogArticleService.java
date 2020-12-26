3fbbf83553205fbee135c319419d2888a9182e42
Jiahao <1332661444@qq.com>
2020-12-26 20:32:12 +0800

    增加部分mybatis的代码写法

--- eladmin-system/src/main/java/me/zhengjie/modules/test/service/BlogArticleService.java
+++ eladmin-system/src/main/java/me/zhengjie/modules/test/service/BlogArticleService.java
@@ -1,9 +1,13 @@
 package me.zhengjie.modules.test.service;
 
-import me.zhengjie.modules.test.domain.BlogArticle;
 import com.baomidou.mybatisplus.extension.service.IService;
+import me.zhengjie.modules.test.domain.BlogArticle;
 
 public interface BlogArticleService extends IService<BlogArticle> {
+
+
+
+
 
 
 }
